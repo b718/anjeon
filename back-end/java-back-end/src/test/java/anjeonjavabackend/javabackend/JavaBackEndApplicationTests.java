a7183b60a2cb7f94b80d5db1d420a84eddbef1e3
whiffy <bryanzhao168@yahoo.ca>
2024-04-30 17:43:28 -0700

    Apr. 30th: - Added the beginning of the Java back-end, sending to github!

---
+++ back-end/java-back-end/src/test/java/anjeonjavabackend/javabackend/JavaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package anjeonjavabackend.javabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
