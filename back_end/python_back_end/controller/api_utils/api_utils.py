b065ff8a3f46aa881b68fc55e162ceba80398b7c
whiffy <bryanzhao168@yahoo.ca>
2024-06-02 18:05:02 -0700

    jun. 2nd, 2024: - added connection to s3 bucket to store new items. - changed tests so type matches. - added new csv function for python back-end.

--- back_end/python_back_end/controller/api_utils/api_utils.py
+++ back_end/python_back_end/controller/api_utils/api_utils.py
@@ -1,5 +1,7 @@
 import collections
-
+import datetime
+import csv
+import os
 class ApiUtilities:
     def getProbabilites(data, modelList):
         analysisTextMap = collections.defaultdict(str)
@@ -8,8 +10,31 @@ class ApiUtilities:
         for model in modelList:
             probabilityOfBullying += model.predict(data)[0]
 
-        probabilityOfBullying /= len(modelList) 
+        # probabilityOfBullying /= len(modelList) 
         analysisTextMap["text"] = data
         analysisTextMap["not_bullying_probability"] = '{:.2f}'.format((1 - probabilityOfBullying) * 100) + "%"
         analysisTextMap["bullying_probability"] = '{:.2f}'.format(probabilityOfBullying * 100) + "%"
         return analysisTextMap
+
+    def createCSVFile(analysisTextMap, s3):
+        '''
+        analysisTextMap["text"] = data
+        analysisTextMap["not_bullying_probability"] = '{:.2f}'.format((1 - probabilityOfBullying) * 100) + "%"
+        analysisTextMap["bullying_probability"] = '{:.2f}'.format(probabilityOfBullying * 100) + "%"
+        '''
+        
+        headers = ["tweet_text","cyberbullying_type"]
+        fileName = f"text_{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+        result = "not_cyberbullying" if float(analysisTextMap["not_bullying_probability"][:-1]) >= 70 else "cyberbullying"
+
+        with open(f"{fileName}.csv", "w") as file:
+            bullyText = csv.writer(file)
+            bullyText.writerow(headers)
+            bullyText.writerow([analysisTextMap["text"], result])
+        
+        s3.upload_file(f"{fileName}.csv", "anjeon-text-bucket", f"{fileName}.csv")
+        os.remove(f"{fileName}.csv")
+        
+        
+
+        
--- back_end/python_back_end/controller/controller.py
+++ back_end/python_back_end/controller/controller.py
@@ -1,5 +1,6 @@
 import flask
 import collections
+import boto3
 from machine_learning import (words_to_features, svm_rbf, random_forests, logistic_regression)
 from api_utils import (api_utils)
 from flask_cors import CORS, cross_origin
@@ -10,20 +11,24 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
 def startServer():
+    global s3
+    s3 = boto3.client('s3')
+    print("S3 Done")
+
     wordToFeatures = words_to_features.WordsToFeatures()
     print("Words To Features Done")
 
-    global randomForests 
-    randomForests = random_forests.RandomForests(wordToFeatures.X_train, wordToFeatures.y_train)
-    print("Random Forests Done")
+    # global randomForests 
+    # randomForests = random_forests.RandomForests(wordToFeatures.X_train, wordToFeatures.y_train)
+    # print("Random Forests Done")
 
-    global logisticRegression
-    logisticRegression = logistic_regression.LogisticRegression(wordToFeatures.X_train, wordToFeatures.y_train)
-    print("Logistic Regression Done")
+    # global logisticRegression
+    # logisticRegression = logistic_regression.LogisticRegression(wordToFeatures.X_train, wordToFeatures.y_train)
+    # print("Logistic Regression Done")
 
-    global svmRbf
-    svmRbf = svm_rbf.SvmRbf(wordToFeatures.X_train, wordToFeatures.y_train)    
-    print("SVM RBF Done")
+    # global svmRbf
+    # svmRbf = svm_rbf.SvmRbf(wordToFeatures.X_train, wordToFeatures.y_train)    
+    # print("SVM RBF Done")
 
     return "Anjeon Python On!"
 
@@ -31,9 +36,11 @@ def startServer():
 @app.route("/analyze-probability", methods=["POST"])
 def analyzeProbability():
     data = flask.request.get_json()["text"]
-    models = [randomForests, logisticRegression, svmRbf]
+    # models = [randomForests, logisticRegression, svmRbf]
+    models = []
     JSONMap = collections.defaultdict(list)
     JSONMap["analysis"] = api_utils.ApiUtilities.getProbabilites(data, models)
+    api_utils.ApiUtilities.createCSVFile(JSONMap["analysis"], s3)
     return flask.jsonify(JSONMap), 200
 
 if (__name__ == "__main__"):
